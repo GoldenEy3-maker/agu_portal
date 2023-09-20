8b634b614e0e60ed6e0cd33e9423b6308753feef
Danil <danil-danil-korolev@bk.ru>
2023-09-20 22:26:29 +0700

    calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,64 +1,52 @@
 import dayjs from "dayjs"
 import { useRouter } from "next/router"
-import { useMemo } from "react"
+import { useMemo, useState } from "react"
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi"
-import { toUpperInitialLetter } from "~/utils/func"
+import { cls, toUpperInitialLetter } from "~/utils/func"
+import { ValueOf } from "~/utils/types"
 import Button from "../Button"
 import styles from "./styles.module.scss"
+
+const Modes = {
+  Days: "days",
+  Months: "months",
+  Years: "years",
+} as const
+
+type Modes = ValueOf<typeof Modes>
 
 type CalendarProps = {
   date?: Date
 }
 
 const Calendar: React.FC<CalendarProps> = (props) => {
-  const router = useRouter()
-  const currentDate = props.date || new Date()
+  const [currentDate, setCurrentDate] = useState(dayjs(props.date) || dayjs())
+  const [selectionMode, setSelectionMode] = useState<Modes>("days")
 
-  const startCurrentMonth = dayjs(currentDate).startOf("month")
-  const endCurrentMonth = dayjs(currentDate).endOf("month")
+  const router = useRouter()
+
+  const startCurrentMonth = currentDate.startOf("month")
+  const endCurrentMonth = currentDate.endOf("month")
   const diffDaysCurrentMonth =
     endCurrentMonth.diff(startCurrentMonth, "day") + 1
 
-  const nextMonth = dayjs(currentDate).add(1, "month")
+  const nextMonth = currentDate.add(1, "month")
   const startNextMonth = nextMonth.startOf("month")
-  const prevMonth = dayjs(currentDate).subtract(1, "month")
+  const prevMonth = currentDate.subtract(1, "month")
   const endPrevMonth = prevMonth.endOf("month")
 
-  const prefix = useMemo(() => {
+  const days = useMemo(() => {
+    const res: dayjs.Dayjs[] = []
     let weekDayStartCurrentMonth = startCurrentMonth.day() - 1
+    let weekDayEndCurrentMonth = 7 - endCurrentMonth.day()
+    let isNotFull = true
+    let suffixCount = 0
 
     if (weekDayStartCurrentMonth === 0) weekDayStartCurrentMonth = 0
     if (weekDayStartCurrentMonth < 0) weekDayStartCurrentMonth = 6
 
-    const res: dayjs.Dayjs[] = []
-    let count =
+    let prefixCount =
       endPrevMonth.date() - (endPrevMonth.date() - weekDayStartCurrentMonth + 1)
-
-    for (
-      let i = endPrevMonth.date() - weekDayStartCurrentMonth + 1;
-      i <= endPrevMonth.date();
-      i++
-    ) {
-      res.push(endPrevMonth.subtract(count, "day"))
-      count--
-    }
-
-    return res
-  }, [])
-
-  const current = useMemo(() => {
-    const res: dayjs.Dayjs[] = []
-
-    for (let i = startCurrentMonth.date(); i <= endCurrentMonth.date(); i++) {
-      res.push(startCurrentMonth.add(i - 1, "day"))
-    }
-
-    return res
-  }, [])
-
-  const suffix = useMemo(() => {
-    let weekDayEndCurrentMonth = 7 - endCurrentMonth.day()
-    let isNotFull = true
 
     if (startCurrentMonth.day() === 6 && diffDaysCurrentMonth >= 30)
       isNotFull = false
@@ -69,74 +57,133 @@ const Calendar: React.FC<CalendarProps> = (props) => {
     if (startCurrentMonth.day() === 5 && diffDaysCurrentMonth === 31)
       isNotFull = false
 
-    const res: dayjs.Dayjs[] = []
-    let count = 0
+    for (
+      let i = endPrevMonth.date() - weekDayStartCurrentMonth + 1;
+      i <= endPrevMonth.date();
+      i++
+    ) {
+      res.push(endPrevMonth.subtract(prefixCount, "day"))
+      prefixCount--
+    }
+
+    for (let i = startCurrentMonth.date(); i <= endCurrentMonth.date(); i++) {
+      res.push(startCurrentMonth.add(i - 1, "day"))
+    }
 
     for (
       let i = startNextMonth.date();
       i < startNextMonth.date() + weekDayEndCurrentMonth + (isNotFull ? 7 : 0);
       i++
     ) {
-      res.push(startNextMonth.add(count, "day"))
-      count++
+      res.push(startNextMonth.add(suffixCount, "day"))
+      suffixCount++
     }
 
     return res
-  }, [])
+  }, [currentDate])
 
   return (
     <div>
       <header className={styles.header}>
-        <Button type="button" className={styles.headerCurr}>
-          {toUpperInitialLetter(
-            new Intl.DateTimeFormat(router.locale, {
-              month: "long",
-              year: "numeric",
-            }).format(currentDate)
-          )}
+        <Button
+          type="button"
+          className={styles.headerCurr}
+          onClick={() =>
+            selectionMode === "days"
+              ? setSelectionMode("months")
+              : setSelectionMode("years")
+          }
+        >
+          <p>
+            {(() => {
+              if (selectionMode === "months") return currentDate.year()
+
+              if (selectionMode === "years") return "2020-2024"
+
+              return toUpperInitialLetter(
+                new Intl.DateTimeFormat(router.locale, {
+                  month: "long",
+                  year: "numeric",
+                }).format(currentDate.toDate())
+              )
+            })()}
+          </p>
         </Button>
         <div className={styles.headerActions}>
-          <Button asIcon type="button">
+          <Button
+            asIcon
+            type="button"
+            onClick={() => {
+              if (selectionMode === "months")
+                return setCurrentDate((prev) => prev.subtract(1, "year"))
+              setCurrentDate((prev) => prev.subtract(1, "month"))
+            }}
+          >
             <BiLeftArrowAlt />
           </Button>
-          <Button asIcon type="button">
+          <Button
+            asIcon
+            type="button"
+            onClick={() => {
+              if (selectionMode === "months")
+                return setCurrentDate((prev) => prev.add(1, "year"))
+              setCurrentDate((prev) => prev.add(1, "month"))
+            }}
+          >
             <BiRightArrowAlt />
           </Button>
         </div>
       </header>
-      <div className={styles.grid}>
-        <span>Пн</span>
-        <span>Вт</span>
-        <span>Ср</span>
-        <span>Чт</span>
-        <span>Пт</span>
-        <span>Сб</span>
-        <span>Вс</span>
-        {prefix.map((date) => (
-          <Button
-            key={date.date()}
-            asIcon
-            type="button"
-            className={styles._notCurrentMonth}
-          >
-            {date.date()}
-          </Button>
-        ))}
-        {current.map((date) => (
-          <Button key={date.date()} asIcon type="button">
-            {date.date()}
-          </Button>
-        ))}
-        {suffix.map((date) => (
-          <Button
-            key={date.date()}
-            type="button"
-            asIcon
-            className={styles._notCurrentMonth}
-          >
-            {date.date()}
-          </Button>
-        ))}
+      <div className={styles.main}>
+        {(() => {
+          if (selectionMode === "months")
+            return (
+              <div className={styles.monthsGrid}>
+                <Button>Январь</Button>
+                <Button>Февраль</Button>
+                <Button>Март</Button>
+                <Button>Апрель</Button>
+                <Button>Май</Button>
+                <Button>Июнь</Button>
+                <Button>Июль</Button>
+                <Button>Август</Button>
+                <Button>Сентябрь</Button>
+                <Button>Октябрь</Button>
+                <Button>Ноябрь</Button>
+                <Button>Декабрь</Button>
+              </div>
+            )
+
+          return (
+            <>
+              <div className={styles.weeks}>
+                <span>Пн</span>
+                <span>Вт</span>
+                <span>Ср</span>
+                <span>Чт</span>
+                <span>Пт</span>
+                <span>Сб</span>
+                <span>Вс</span>
+              </div>
+              <div className={styles.daysGrid}>
+                {days.map((date) => (
+                  <Button
+                    key={date.unix()}
+                    asIcon
+                    type="button"
+                    className={cls([], {
+                      [styles._notCurrentMonth ?? ""]:
+                        date.month() !== currentDate.month(),
+                      [styles._currentDay ?? ""]: date.isSame(dayjs(), "date"),
+                    })}
+                  >
+                    {date.date()}
+                  </Button>
+                ))}
+              </div>
+            </>
+          )
+        })()}
       </div>
     </div>
   )
