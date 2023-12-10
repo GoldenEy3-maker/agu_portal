a3f3ce0b2e1141cacb1c93bd2c1518e2f9980394
GoldenEy3-maker <danil-danil-korolev@bk.ru>
2023-12-10 21:02:45 +0700

    prod server add dir to props

--- src/server/prodServer.ts
+++ src/server/prodServer.ts
@@ -10,7 +10,7 @@ import { createTRPCContext } from "./api/trpc"
 const port = parseInt(process.env.NEXT_PUBLIC_PORT || "3000", 10)
 const hostname = process.env.NEXT_PUBLIC_APP_HOSTNAME ?? "127.0.0.1"
 const dev = process.env.NODE_ENV !== "production"
-const app = next({ dev, port, hostname })
+const app = next({ dev, port, hostname, dir: process.cwd() })
 const handle = app.getRequestHandler()
 
 void app.prepare().then(() => {
